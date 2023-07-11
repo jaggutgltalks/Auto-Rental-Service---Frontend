89e31f5e86393379a140344317abdf97635bf69d
Jaggu <kishanyadavrajput@gmail.com>
2023-07-11 17:34:42 +0530

    Auto Rental Service

--- startingJs/test.js
+++ startingJs/test.js
@@ -17,8 +17,10 @@ function initAutocomplete(){
             // componentRestrictions : {'state' : ['Uttar Pradesh']},
             fields : ['geometry']
         });
+        // definition of addListener to be checked
         autocomplete1.addListener('place_changed',onPlaceChanged1);
         autocomplete2.addListener('place_changed',onPlaceChanged2);
+        // code();
 }
 
 var latitude1;
@@ -32,23 +34,26 @@ function onPlaceChanged1(){
     else{
         latitude1 = place1.geometry.location.lat();
         longitude1= place1.geometry.location.lng();
-        initialize();
+        // initialize(); //map popup
     }
 }
+// check this window.init
 window.initMap = onPlaceChanged1;
 
 var latitude2;
 var longitude2;
 
-function onPlaceChanged2(){
+ function onPlaceChanged2(){
     var place2 = autocomplete2.getPlace();
     if(!place2.geometry){
         document.getElementById('autocomplete2').placeholder = 'Enter a valid place';
     }
     else{
         // document.getElementById('details').innerHTML = place2.name;
-        latitude2 = place2.geometry.location.lat();
-        longitude2= place2.geometry.location.lng(); 
+        // await to be tested
+        latitude2 =  place2.geometry.location.lat();
+        longitude2=  place2.geometry.location.lng();
+        // code();
     }
 }
 
@@ -73,10 +78,17 @@ function initialize() {
   }
   // Create Map
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  document.getElementById('mapClass').style.display = 'flex'
+  document.getElementById('map').style.display = 'flex'
+ 
+ // can be removed
   google.maps.event.addListener(map, 'click', function() {
     infowindow.close();
   });
 }
+
+
 
 var pos1;
 var pos2;
@@ -96,71 +108,139 @@ var directionDisplay;
 
 // directionDisplay.setMap(map);
 function code(){
+
+  
+
+     initialize(); //map popup
+
     pos1 = new google.maps.LatLng(latitude1,longitude1);
     pos2 = new google.maps.LatLng(latitude2,longitude2);
-    if (marker1) marker1.setMap(null);
-    if (marker2) marker2.setMap(null);
-     marker1 = new google.maps.Marker({
-        map: map,
+    // if (marker1) marker1.setMap(null);
+    // if (marker2) marker2.setMap(null);
+    //  marker1 = new google.maps.Marker({
+    //     map: map,
+    //     draggable: true,
+    //     // position: new google.maps.LatLng(latitude1,longitude1)
+    //     position: pos1
+    //   });
+    //  marker2 = new google.maps.Marker({
+    //     map: map,
+    //     draggable: true,
+    //     // position: new google.maps.LatLng(latitude2,longitude2)
+    //     position: pos2
+    //   });
+    //   google.maps.event.addListener(marker1, 'dragend', function() {
+    //     latitude1 = marker1.getPosition().lat();
+    //     longitude1 = marker1.getPosition().lng();
+    //   });
+    //   google.maps.event.addListener(marker2, 'dragend', function() {
+    //     latitude2 = marker2.getPosition().lat();
+    //     longitude2 = marker2.getPosition().lng();
+    //   });
+   
+
+      directionsService = new google.maps.DirectionsService();
+      directionsRenderer = new google.maps.DirectionsRenderer({
         draggable: true,
-        // position: new google.maps.LatLng(latitude1,longitude1)
-        position: pos1
+        map 
       });
-     marker2 = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        // position: new google.maps.LatLng(latitude2,longitude2)
-        position: pos2
+
+      directionsRenderer.addListener("directions_changed", () => {
+        const directions = directionsRenderer.getDirections();
+        if (directions) {
+          computeTotalDistance(directions);
+        }
       });
-      google.maps.event.addListener(marker1, 'dragend', function() {
-        latitude1 = marker1.getPosition().lat();
-        longitude1 = marker1.getPosition().lng();
-      });
-      google.maps.event.addListener(marker2, 'dragend', function() {
-        latitude2 = marker2.getPosition().lat();
-        longitude2 = marker2.getPosition().lng();
-      });
-      directionService = new google.maps.DirectionsService();
-      directionDisplay = new google.maps.DirectionsRenderer();
-      directionDisplay.setMap(map);
-      calcRoute();
+      displayRoute(
+        pos1,
+        pos2,
+        directionsService,
+        directionsRenderer
+      );
+      // calcRoute();
+
+      window.scrollTo(0,0);
+       window.addEventListener("scroll",code);
+      
+    
+}
+
+function displayRoute(origin, destination, service, display) {
+  service
+    .route({
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+      avoidTolls: true,
+    })
+    .then((result) => {
+      display.setDirections(result);
+    })
+    .catch((e) => {
+      alert("Could not display directions due to: " + e);
+    });
+}
+var distance;
+var duration;
+
+function computeTotalDistance(result) {
+  let totalDistance = 0;
+  let totalDuration = 0;
+
+  const myroute = result.routes[0];
+
+  if (!myroute) {
+    return;
+  }
+
+  for (let i = 0; i < myroute.legs.length; i++) {
+    totalDistance += myroute.legs[i].distance.value;
+    totalDuration += myroute.legs[i].duration.value;
+  }
+
+  totalDistance = totalDistance / 1000;
+  distance = totalDistance;
+  duration = totalDuration;
+  console.log(totalDistance+" kmss");
+  console.log(totalDuration+" seconds");
 }
 var cost;
+
 // functionality
  async function calcRoute() {
         // create Request
         var myDate = new Date("2023-12-30T11:00:00+0000");
         var fiDate = Date.parse(myDate);
         console.log(fiDate);
-        var request = {
-            origin: pos1,
-            destination: pos2,
-            travelMode: google.maps.TravelMode.DRIVING,
-            drivingOptions: {
-                departureTime: new Date(fiDate),
-                trafficModel: 'optimistic'
-              },
-            unitSystem: google.maps.UnitSystem.METRIC
-          }
-          console.log(request);
+        // var request = {
+        //     origin: pos1,
+        //     destination: pos2,
+        //     travelMode: google.maps.TravelMode.DRIVING,
+        //     drivingOptions: {
+        //         departureTime: new Date(fiDate),
+        //         trafficModel: 'optimistic'
+        //       },
+        //     unitSystem: google.maps.UnitSystem.METRIC
+        //   }
+        //   console.log(request);
           // Nandanagar
           // Mohaddipur
 
           // Passing request to the route method
-          var distance;
-          var time;
-          await directionService.route(request, (result) => {
-            if(result != null){
+          // var distance;
+          // var time;
+          // await directionService.route(request, (result) => {
+          //   if(result != null){
                 
-                // getting distance & time
+          //       // getting distance & time
 
-                distance = result.routes[0].legs[0].distance.value;
-                console.log(distance);
-                time = result.routes[0].legs[0].duration.value;
-                console.log(time);
-            }
-          })
-          await fetch(`http://localhost:8080/customer/cost?distance=${distance}&duration=${time}`,{
+          //       distance = result.routes[0].legs[0].distance.value;
+          //       console.log(distance/1000 +" km");
+          //       time = result.routes[0].legs[0].duration.value;
+          //       console.log(time);
+          //   }
+          // })
+          await fetch(`http://localhost:8080/customer/cost?distance=${distance}&duration=${duration}`,{
           method : 'GET'
           })
           .then(res => res.json() )
@@ -168,4 +248,13 @@ var cost;
     }
 
 
+    // jaggu
+    let p = fetch('http://localhost:8080/client/calculate-cost?distance=5&duration=10')
+    p.then((response) => {
+      console.log(response.status)
+      console.log(response.ok)
+      return response.json();
+    }).then((value2) => {
+      console.log(value2)
+    })
 
